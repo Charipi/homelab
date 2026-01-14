60c12fbacce8d3b50adb703c3390ad0286ab52c6
Charipi <cburometto3@gmail.com>
2026-01-13 19:43:25 -0500

    Standardized to use file paths without / Added requirements.txt for easy pip install

--- calls.py
+++ calls.py
@@ -4,7 +4,7 @@ config = dotenv_values(".env")
 
 def execute_remote_python(script_path: str, args: list) -> bool:
     try:
-        cmd = f"sudo {config['RASPI_PATH']}.venv/bin/python3 {script_path} {' '.join(args)} > /dev/null 2>&1 &"
+        cmd = f"sudo {config['RASPI_PATH']}/.venv/bin/python3 {script_path} {' '.join(args)} > /dev/null 2>&1 &"
         subprocess.run(["ssh", f"{config['RASPI_USER']}@{config['RASPI_HOST']}", "sudo pkill -9 python"])
         subprocess.run(["ssh", f"{config['RASPI_USER']}@{config['RASPI_HOST']}", cmd], timeout=10)
         return True
