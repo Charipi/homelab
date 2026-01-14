3858e46572ce195bb391e545a42ca1daa3574376
Charipi <cburometto3@gmail.com>
2026-01-13 20:58:35 -0500

    Fixed paths in voice client

--- VoiceClient/agent.py
+++ VoiceClient/agent.py
@@ -8,19 +8,19 @@ config = dotenv_values(".env")
 
 def off():
     """Turn off the LED strip. Returns the success value."""
-    return execute_remote_python(script_path=f'{config["LIGHT_SERVER_REPO_PATH"]}/solid.py', args=["0", "0", "0"])
+    return execute_remote_python(script_path=f'{config["LIGHT_SERVER_REPO_PATH"]}/LightServer/solid.py', args=["0", "0", "0"])
 
 @tool
 def set_color(red: int, green: int, blue: int) -> str:
     """Set the LED strip to be a solid rgb color with values between 0 and 255. Returns the success value."""
-    return execute_remote_python(script_path=f'{config["LIGHT_SERVER_REPO_PATH"]}/solid.py', args=[str(red), str(green), str(blue)])
+    return execute_remote_python(script_path=f'{config["LIGHT_SERVER_REPO_PATH"]}/LightServer/solid.py', args=[str(red), str(green), str(blue)])
 
 @tool
 def set_color_loop(primary_red: int, primary_green: int, primary_blue: int, secondary_red: int, secondary_green: int, secondary_blue: int) -> str:
     """Set the LED strip to cycle the primary rgb color around the secondary rgb color,
     with the primary color being the first three arguments and the secondary color being the next three arguments.
     All rgb values are between 0 and 255. Returns the success value."""
-    return execute_remote_python(script_path=f'{config["LIGHT_SERVER_REPO_PATH"]}/loop.py', args=[str(primary_red), str(primary_green), str(primary_blue), str(secondary_red), str(secondary_green), str(secondary_blue), "20", "1"])
+    return execute_remote_python(script_path=f'{config["LIGHT_SERVER_REPO_PATH"]}/LightServer/loop.py', args=[str(primary_red), str(primary_green), str(primary_blue), str(secondary_red), str(secondary_green), str(secondary_blue), "20", "1"])
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash", 
